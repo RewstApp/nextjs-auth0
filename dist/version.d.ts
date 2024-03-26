24ee8b08461247299e804d1b8ad41816c2b7a8d6
Pavan Kumar <pavan.dinesh@rewst.io>
2024-03-26 15:57:08 -0400

    add build dist

---
+++ dist/version.d.ts
@@ -0,0 +1,3 @@
+declare const _default: "2.6.3";
+export default _default;
+//# sourceMappingURL=version.d.ts.map
---
+++ dist/version.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = '2.6.3';
+//# sourceMappingURL=version.js.map
